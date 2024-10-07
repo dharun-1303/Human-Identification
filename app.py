0de9dcd5f392be158cd010ae610ecdf148e33d04
dharun-1303 <145691464+dharun-1303@users.noreply.github.com>
2024-10-07 22:23:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-
-# Rest of the code
-
-
 # Load the trained model
 model = tf.keras.models.load_model('/content/drive/MyDrive/datasets/human_detection_model_dataset.h5')
 
@@ -19,26 +15,18 @@ def preprocess_image(img, target_size=(128, 128)):
     img_array = img_array / 255.0  # Normalize the image
     return img_array
 
-# Streamlit app
+# Streamlit app code
 st.title("Human Detection Application")
-
 st.write("Upload an image to check if it contains a human.")
-
-# Upload image
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Display the uploaded image
     img = Image.open(uploaded_file)
     st.image(img, caption='Uploaded Image', use_column_width=True)
     
-    # Preprocess the image
     img_array = preprocess_image(img)
-    
-    # Make prediction
     prediction = model.predict(img_array)
     
-    # Display result
     if prediction[0] > 0.5:
         st.write("Prediction: Human Detected")
     else:
