c0dd04f3f722263ce099eedbb947f4062b706315
dharun-1303 <145691464+dharun-1303@users.noreply.github.com>
2024-10-07 23:23:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import gdown
+import os
 
-# Load the trained model
-model = tf.keras.models.load_model('/content/drive/MyDrive/datasets/human_detection_model_dataset.h5')
+# Function to download model from Google Drive
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model_path = 'human_detection_model_dataset.h5'
+    
+    # Check if the model file already exists
+    if not os.path.exists(model_path):
+        # Replace 'your_model_id' with the actual file ID from Google Drive
+        url = 'https://drive.google.com/file/d/1-4DeYBQeBDhpQcDaOiRUGqYKFqjGJdKl/view?usp=drive_link'
+        gdown.download(url, model_path, quiet=False)
+    
+    # Load the model after downloading
+    model = tf.keras.models.load_model(model_path)
+    return model
+
+# Call the load_model function to load the model
+model = load_model()
 
 # Function to preprocess the input image
 def preprocess_image(img, target_size=(128, 128)):
@@ -23,10 +40,14 @@ if uploaded_file is not None:
     img = Image.open(uploaded_file)
     st.image(img, caption='Uploaded Image', use_column_width=True)
     
-    img_array = preprocess_image(img)
-    prediction = model.predict(img_array)
-    
-    if prediction[0] > 0.5:
-        st.write("Prediction: Human Detected")
+    if model is not None:
+        img_array = preprocess_image(img)
+        prediction = model.predict(img_array)
+        
+        # Adjust the threshold here if needed
+        if prediction[0] > 0.5:
+            st.write("Prediction: Human Detected")
+        else:
+            st.write("Prediction: No Human Detected")
     else:
-        st.write("Prediction: No Human Detected")
+        st.write("Model not available.")
